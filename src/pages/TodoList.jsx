6fd577bdb5d5e7d6d73991ca85691e9494f41e57
MohsenKazemizade <mohsenkazemizade.dev@gmail.com>
2025-01-29 15:59:26 +0330

    feat: done with todolist

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,6 +1,53 @@
-import TodosItems from "../components/TodosItems";
+import { useRef, useState } from "react";
+import TodosItem from "../components/TodosItem";
 
 export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [errorText, setErrorText] = useState("");
+  const input = useRef(null);
+
+  function addTask() {
+    let task = input.current.value;
+    if (task.trim().length === 0) {
+      setErrorText("Empty task is not allowed!");
+      input.current.value = "";
+      return;
+    }
+    if (task.trim().length < 3) {
+      setErrorText("Task must be more than 3 characters!");
+      input.current.value = "";
+      return;
+    }
+    setErrorText("");
+    setTodos([
+      ...todos,
+      {
+        title: task.trim(),
+        isDone: false,
+        id: new Date().getTime(),
+      },
+    ]);
+    input.current.value = "";
+  }
+  function removeTask(id) {
+    const newTodos = todos.filter(function (item) {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  }
+  function doneTask(id) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  }
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      addTask();
+      // input.current.value = "";
+    }
+  }
   return (
     <>
       <div className="background__circle"></div>
@@ -9,11 +56,19 @@ export default function TodoList() {
           <h1>My ToDo List</h1>
           <div className="item" id="taskForm">
             <div className="checkbox"></div>
-            <input type="text" name="todoInput" id="todoInput" />
-            <button id="addBtn">+</button>
+            <input
+              type="text"
+              name="todoInput"
+              id="todoInput"
+              ref={input}
+              onKeyDown={handleKeyDown}
+            />
+            <button id="addBtn" onClick={addTask}>
+              +
+            </button>
           </div>
-          <p id="error"></p>
-          <TodosItems />
+          <p id="error">{errorText}</p>
+          <TodosItem data={todos} removeTask={removeTask} doneTask={doneTask} />
 
           <div className="clip__top">
             <div className="clip__hole"></div>
